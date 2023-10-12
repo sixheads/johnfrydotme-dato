b0928bdf476fd37261fe2dc1158ca97715ecda98
John Fry <john@sixheads.com>
2023-10-12 11:28:54 +1100

    Create Header and Nav components

---
+++ components/Nav.jsx
@@ -0,0 +1,31 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NAV = [
+  { href: '/', label: 'Work' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
+];
+
+export default function Nav() {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  return (
+    <nav role="navigation">
+      <ul className="flex gap-5">
+        {NAV.map(({ href, label }) => {
+          const isActive = pathname === href;
+          return (
+            <li key={href}>
+              <Link href={href} className={isActive ? 'active' : ''}>
+                {label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
